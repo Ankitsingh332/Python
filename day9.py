22699e4352969fccb6feae8c29ed1522afb5a360
Ankit Kumar singh <70365778+Ankitsingh332@users.noreply.github.com>
2024-11-20 23:42:01 +0530

    Add files via upload

---
+++ day9.py
@@ -0,0 +1,14 @@
+
+# Typecasting........
+
+string="12"
+number=7
+c=int(string)
+print("The sum of",string,"and",number,"is",c+number)
+
+s="12"
+n=7
+a=7.5
+print(type(s))
+print(type(n))
+print(type(a))
