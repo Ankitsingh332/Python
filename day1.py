da5278a4f1f0af4675450f079f362535b3c252bc
Ankit Kumar singh <70365778+Ankitsingh332@users.noreply.github.com>
2024-11-20 22:50:59 +0530

    Python

---
+++ day1.py
@@ -0,0 +1,5 @@
+print("hii")
+print("5+6")
+print(5+6)
+a=1
+print(type(a))
---
+++ day7.py
@@ -0,0 +1,8 @@
+#Create a calculator capable of performing addition, subtraction, multiplication and division operations on two numbers. Your program should format the output in a readable manner!
+
+a=20
+b=10
+print("The value of",a,"+",b,"=",a+b)
+print("The value of",a,"-",b,"=",a-b)
+print("The value of",a,"*",b,"=",a*b)
+print("The value of",a,"/",b,"=",a/b)
